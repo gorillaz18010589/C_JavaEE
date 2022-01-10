f88eaa0f530194db705ac181fbbad110ac0afcb5
hank <gorillaz1801058@gmail.com>
2022-01-10 21:41:39 +0800

    2022/01/10 //目的html等input取得參數方法,二選一,多選,郵遞區號下拉選單,文字選單java Test05,html->test04

--- message/src/newtest/Test05_Radio.java
+++ message/src/newtest/Test05_Radio.java
@@ -12,6 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 //javax.servlet.ServletRequest.getParameterValues(String name):多選題時可用,取得多個參數時(回傳String[])
 //java.io.PrintWriter.write(String s):輸出串流檔案(void)
 //test04.html
+//目的html等input取得參數方法,二選一,多選,郵遞區號下拉選單,文字選單
+//
+//後端要能收得到給name這些都是後端再給的
+/*
+ * //1.抓取男女二選一方式
+		if(gender.equals("f")) {
+			printWriter.append("女生");
+		}else {
+			printWriter.append("男生");
+		}
+ * 
+ *  //3.多選題利用getParameterValues,一次取得同一個參數的所有值
+		 String[] hobbys = request.getParameterValues("hobby");
+		 for(String hobbyValue : hobbys) {
+				printWriter.append("選到：" + hobbyValue +"<br>");
+		 }
+ * 
+ * //4.下拉式選單取得郵遞區號方式,我知道你真正想要的是郵遞區號,所以value 401,402,403,404,直接把你想要的值放在vlaue中
+		 String  addressValue = request.getParameter("address");
+		 printWriter.append(addressValue);
+ * 
+ * 
+ *  //5.取得textarea參數內容
+		 String text = request.getParameter("text");
+		 printWriter.append(text);
+		 printWriter.append("<br>");
+ * */
 //
 /**
  * Servlet implementation class Test05_Radio
@@ -36,7 +63,7 @@ public class Test05_Radio extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");//設定標籤內容,網頁html
 		PrintWriter printWriter = response.getWriter();
 		
-		//抓取男女二選一方式
+		//1.抓取男女二選一方式
 		if(gender.equals("f")) {
 			printWriter.append("女生");
 		}else {
@@ -78,6 +105,18 @@ public class Test05_Radio extends HttpServlet {
 		 for(String hobbyValue : hobbys) {
 				printWriter.append("選到：" + hobbyValue +"<br>");
 		 }
+		 
+		 //4.下拉式選單取得郵遞區號方式,我知道你真正想要的是郵遞區號,所以value 401,402,403,404,直接把你想要的值放在vlaue中
+		 String  addressValue = request.getParameter("address");
+		 printWriter.append(addressValue);
+		 printWriter.append("<br>");
+
+		 
+		 //5.取得textarea參數內容
+		 String text = request.getParameter("text");
+		 printWriter.append(text);
+		 printWriter.append("<br>");
+
 
 	}
 
